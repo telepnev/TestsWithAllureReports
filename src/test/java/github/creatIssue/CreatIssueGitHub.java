c711a3c58b21e67c8d8ef7397bc53648bbd505d1
Evgeniy Telepnev <telepnef@yandex.ru>
2020-11-29 13:07:59 +0300

    Added Lambda Steps

--- src/test/java/github/creatIssue/CreatIssueGitHub.java
+++ src/test/java/github/creatIssue/CreatIssueGitHub.java
@@ -1,27 +1,56 @@
 package github.creatIssue;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
 import credentials.Properties;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.byLinkText;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CreatIssueGitHub {
     Properties cred = new Properties();
     String pass = cred.getPASS();
     String email = cred.getEMAIL();
+    String REPOSITORY = "TestsWithAllureReports";
+    String USER = "telepnev";
 
     @Test
-    public void creatIssue() {
+    @DisplayName("Тест создание Issue через WEB")
+    @Feature("Issues")
+    @Story("Пользователь может создать, назначить, и изменить Issue")
+    @Link(url = "https://github.com/telepnev/TestsWithAllureReports", name = "TestsWithAllureReports")
+    @Owner("Telepnev")
+    @Severity(SeverityLevel.CRITICAL)
+
+    public void creatIssueOnlySelenide() {
 
         open("https://github.com/");
         $(byText("Sign in")).click();
         $("#login_field").val(email);
         $("#password").val(pass);
+        $(byName("commit")).click();
 
-        sleep(3000);
+        $(".header-search-input").val(REPOSITORY).submit();
+        $x("//a[@href='/telepnev/TestsWithAllureReports']").click();
+        $("h1").shouldHave(text(REPOSITORY));
+        $x("//span[@data-content='Issues']").click();
+        $x("//a[@role='button']").click();
+
+        $("#issue_title").val("First title from intellij idea !");
+
+        $("#assignees-select-menu").click();
+        $x("//span[text()='telepnev']").click();
+        $("#assignees-select-menu").click();
+
+        $("#labels-select-menu").click();
+        $x("//span[text()='question']").click();
+        $x("//span[text()='good first issue']").click();
+        $("#labels-select-menu").click();
+        $(byText("Submit new issue")).click();
+
+        $("#partial-discussion-header").shouldHave(text("First title from intellij idea !"));
     }
+
 }
---
+++ src/test/java/github/creatIssue/LambdaSteps.java
@@ -0,0 +1,85 @@
+package github.creatIssue;
+
+import credentials.Properties;
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
+
+public class LambdaSteps {
+
+    Properties cred = new Properties();
+    String pass = cred.getPASS();
+    String email = cred.getEMAIL();
+    String REPOSITORY = "TestsWithAllureReports";
+    String USER = "telepnev";
+    String ISSUE_TITLE = "First title from intellij idea !";
+
+
+    @Test
+    public void creatIssueUseLambdaSteps() {
+        Allure.feature("Issues");
+        Allure.story("Пользователь создает Issue ");
+        Allure.parameter("Repository", REPOSITORY);
+
+        step("Открываем GitHub", () -> {
+            open("https://github.com/");
+        });
+
+        step("Логинемся на сайт", () -> {
+            $(byText("Sign in")).click();
+            $("#login_field").val(email);
+            $("#password").val(pass);
+            $(byName("commit")).click();
+        });
+
+        step("Ищем репозиторий" + REPOSITORY, () -> {
+            $(".header-search-input").val(REPOSITORY).submit();
+        });
+
+        step("Переходим в репозиторий", () -> {
+            $x("//a[@href='/telepnev/TestsWithAllureReports']").click();
+
+        });
+
+        step("проверяем правильность выбранного репозитория", () -> {
+            $("h1").shouldHave(text(REPOSITORY));
+
+        });
+
+        step("Создаем Issues", () -> {
+            $x("//span[@data-content='Issues']").click();
+            $x("//a[@role='button']").click();
+        });
+
+        step("Заполняем Title", () -> {
+            $("#issue_title").val(ISSUE_TITLE);
+        });
+
+        step("Асайним на " + USER, () -> {
+            $("#assignees-select-menu").click();
+            $x("//span[text()='telepnev']").click();
+            $("#assignees-select-menu").click();
+        });
+
+        step("Устанавливаем Labels 'question', 'good first issue'", () -> {
+            $("#labels-select-menu").click();
+            $x("//span[text()='question']").click();
+            $x("//span[text()='good first issue']").click();
+            $("#labels-select-menu").click();
+        });
+
+        step("Кликаем 'Submit new issue'", () -> {
+            $(byText("Submit new issue")).click();
+        });
+
+        step("Проверяем созданое Issue" + ISSUE_TITLE, () -> {
+            $("#partial-discussion-header").shouldHave(text(ISSUE_TITLE));
+        });
+    }
+}
